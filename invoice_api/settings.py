d732a8c1f44d38d3b9c3b8e429426ffa52645a02
Charl <charl@nclose.co.za>
2023-08-09 18:00:50 +0200

    DB update

--- invoice_api/settings.py
+++ invoice_api/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'invoice_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'railway',
+       'USER': 'postgres',
+       'PASSWORD': 'I8ycWB0tuBusBwjVGZOJ',
+       'HOST': 'containers-us-west-153.railway.app',
+       'PORT': '6819',
     }
 }
 
